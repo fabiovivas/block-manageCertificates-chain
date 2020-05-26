1a3627f9b4935a597886a0a32438ef4688a0278c
fabio.vivas <fvivas1982@gmail.com>
2020-05-26 12:22:42 -0300

    basic contract whitout oracle implementation

--- on-chain/contracts/manage-certificate.sol
+++ on-chain/contracts/manage-certificate.sol
@@ -1,4 +1,66 @@
-pragma solidity ^0.6.7;
+pragma experimental ABIEncoderV2;
+pragma solidity ^0.6.8;
+// SPDX-License-Identifier: MIT
 
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+contract ManageCertificate is Ownable {
+    struct Certificate {
+        address studentId;
+        address schoolId;
+        string ipfsId;
+    }
 
-contract ManageCertificate {}
+    mapping(address => bool) public isSchool;
+    mapping(address => bool) public isStudent;
+    mapping(address => mapping(address => bool)) private studentsBySchool;
+    mapping(address => mapping(address => Certificate[])) private certificateByStudents;
+    mapping(address => mapping(address => bool)) public certificateAccess;
+
+    event schoolRegistered(address schoolId);
+    event studentRegistered(address studentId, address schoolId);
+    event CertificateEmited(address studentId, address schoolId, string ipfsId);
+
+    modifier onlySchool() {
+        require(isSchool[msg.sender], "msg.sender is not a school.");
+        _;
+    }
+
+    modifier onlyStudent() {
+        require(isStudent[msg.sender], "msg.sender is not a student.");
+        _;
+    }
+
+    function registerSchool(address schoolId) public onlyOwner {
+        isSchool[schoolId] = true;
+        emit schoolRegistered(schoolId);
+    }
+
+    function registerStudent(address studentId) public onlySchool {
+        studentsBySchool[msg.sender][studentId] = true;
+        certificateAccess[studentId][msg.sender] = true;
+        isStudent[studentId] = true;
+        emit studentRegistered(studentId, msg.sender);
+    }
+
+    function emiteCertificate(address studentId, string memory ipfsIdDocument) public onlySchool {
+        require(studentsBySchool[msg.sender][studentId], "This student doesn't participate of your school");
+        certificateByStudents[studentId][msg.sender].push(
+            Certificate(studentId, msg.sender, ipfsIdDocument)
+        );
+        emit CertificateEmited(studentId, msg.sender, ipfsIdDocument);
+    }
+
+    function allowAccess(address addr) public onlyStudent {
+        certificateAccess[msg.sender][addr] = true;
+    }
+
+    function removeAccess(address addr) public onlyStudent {
+        certificateAccess[msg.sender][addr] = false;
+    }
+
+    function getCertificates(address studentId, address schoolId) public view returns(Certificate[] memory) {
+        require(certificateAccess[studentId][msg.sender], "You don't have access to this information");
+        Certificate[] memory certificates = certificateByStudents[studentId][schoolId];        
+        return certificates;
+    } 
+}
